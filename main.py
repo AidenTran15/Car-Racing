ec4427713cb5c8d36fd5079b8871ada6faad640b
AidenTran15 <aidenkiettran@gmail.com>
2021-11-14 18:29:42 +1100

    Created Abstarct car class for a car

--- main.py
+++ main.py
@@ -15,13 +15,33 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Racing Game!")
 
 FPS = 60
+
+class AbstractCar:
+    def __init__(self, max_vel, rotatio_vel):
+        self.max_vel = max_vel
+        self.rotatio_vel = rotatio_vel
+        self.angle = 0
+        
+    def rotate(self, left=False, right=False):
+        if left:
+            self.angle += self.rotation_vel
+        elif right:
+            self.angle -= self.rotation_vel
+
+def draw(win, images):
+    for img, pos in images:
+        win.blit(img, pos)
+        
 run = True
 clock = pygame.time.Clock()
+images = [(GRASS, (0,0)), (TRACK, (0,0))]
+
+
+
 while run:
     clock.tick(FPS)
-    WIN.blit(GRASS, (0, 0))
-    WIN.blit(TRACK, (0, 0))
-    WIN.blit(GREEN_CAR, (0, 0))
+    draw(WIN, images)
+    draw(WIN, GREEN_CAR)
     pygame.display.update()
     
     for event in pygame.event.get():
