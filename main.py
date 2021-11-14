865f6ec06c0e1de657cb5d501ad2268701c22f5f
AidenTran15 <aidenkiettran@gmail.com>
2021-11-14 17:02:21 +1100

    Display the grass, track and finish line on the screen

--- main.py
+++ main.py
@@ -12,3 +12,19 @@ GREEN_CAR = pygame.image.load("images/green-car.png")
 WIDTH, HEIGHT = TRACK.get_width(), TRACK.get_height()
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Racing Game!")
+
+FPS = 60
+run = True
+clock = pygame.time.Clock()
+while run:
+    clock.tick(FPS)
+    WIN.blit(GRASS, (0, 0))
+    WIN.blit(TRACK, (0, 0))
+    WIN.blit(FINISH, (0, 0))
+    pygame.display.update()
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+            break
+pygame.quit()
