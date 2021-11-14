59120e3620761705d65a946cfa349fe8c8120924
AidenTran15 <aidenkiettran@gmail.com>
2021-11-14 23:42:00 +1100

    [utils] Added rotate car function

--- utils.py
+++ utils.py
@@ -3,3 +3,7 @@ import pygame
 def scale_image(img, factor):
     size = round(img.get_width() * factor), round(img.get_height() * factor)
     return pygame.transform.scale(img, size)
+
+def blit_rotate_center(win, image, top_left, angle):
+    rotated_image = pygame.tranform.rotate(image, angle)
+    new_rect = rotated_image.get_rect(center=image.get_rect(topleft = top_left).center)
