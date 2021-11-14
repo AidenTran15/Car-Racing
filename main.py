988755e958464f7d79fc696dacdaef9661e2426f
AidenTran15 <aidenkiettran@gmail.com>
2021-11-14 17:12:01 +1100

    Resize image to fit the screen

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import time
 import math
 from utils import scale_image
 
-GRASS = pygame.image.load("images/grass.jpg")
-TRACK = pygame.image.load("images/track.png")
-TRACK_BORDER = pygame.image.load("images/track-border.png")
+GRASS = scale_image(pygame.image.load("images/grass.jpg"), 2.5)
+TRACK = scale_image(pygame.image.load("images/track.png"), 0.9)
+TRACK_BORDER = scale_image(pygame.image.load("images/track-border.png"), 0.9)
 FINISH = pygame.image.load("images/finish.png")
-RED_CAR = pygame.image.load("images/red-car.png")
-GREEN_CAR = pygame.image.load("images/green-car.png")
+RED_CAR = scale_image(pygame.image.load("images/red-car.png"), 0.55)
+GREEN_CAR = scale_image(pygame.image.load("images/green-car.png"), 0.55)
 
 WIDTH, HEIGHT = TRACK.get_width(), TRACK.get_height()
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -21,7 +21,7 @@ while run:
     clock.tick(FPS)
     WIN.blit(GRASS, (0, 0))
     WIN.blit(TRACK, (0, 0))
-    WIN.blit(FINISH, (0, 0))
+    WIN.blit(GREEN_CAR, (0, 0))
     pygame.display.update()
     
     for event in pygame.event.get():
