ffa5323b9bbf9b2e3f9920daf2eba642d7fd529f
AidenTran15 <aidenkiettran@gmail.com>
2021-11-16 17:08:29 +1100

    Moving the car by an angle

--- main.py
+++ main.py
@@ -20,18 +20,38 @@ class AbstractCar:
     def __init__(self, max_vel, rotation_vel):
         self.img = self.IMG
         self.max_vel = max_vel
+        self.vel = 0
         self.rotation_vel = rotation_vel
         self.angle = 180
         self.x, self.y = self.START_POS
+        self.acceleration = 0.1
         
+    # Rotate the car    
     def rotate(self, left=False, right=False):
         if left:
             self.angle += self.rotation_vel
         elif right:
             self.angle -= self.rotation_vel
         
+        
     def draw(self, win):
         blit_rotate_center(win, self.img, (self.x, self.y), self.angle)
+        
+    def move_forward(self):
+        self.vel = min(self.vel + self.acceleration, self.max_vel)
+        self.move()
+        
+    def move(self):
+        radians = math.radians(self.angle)
+        vertical = math.cos(radians) * self.vel
+        horizontal = math.sin(radians) * self.vel
+        
+        self.y -= vertical
+        self.x -= horizontal
+        
+    def reduce_speed(self):
+        self.vel = max(self.vel - self.acceleration / 2, 0)
+        self.move()
         
 class PlayerCar(AbstractCar):
     IMG = GREEN_CAR
@@ -47,7 +67,7 @@ def draw(win, images, player_car):
 run = True
 clock = pygame.time.Clock()
 images = [(GRASS, (0,0)), (TRACK, (0,0))]
-player_car = PlayerCar(4, 4)
+player_car = PlayerCar(4, 2)
 
 
 
@@ -62,8 +82,15 @@ while run:
             break
         
     keys = pygame.key.get_pressed()
+    moved = False
+    
     if keys[pygame.K_a]:
         player_car.rotate(left=True)
     if keys[pygame.K_d]:
         player_car.rotate(right=True)
+    if keys[pygame.K_w]:
+        moved = True
+        player_car.move_forward()
+    if not moved:
+        player_car.reduce_speed()
 pygame.quit()
