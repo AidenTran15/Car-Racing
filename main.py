733a05ea1a1d7ae435ce3a0a4ce394aa84fc60ca
AidenTran15 <aidenkiettran@gmail.com>
2021-11-29 13:11:54 +1100

    Fixed computer car can not run with the path
    Now the car run follow the the point path

--- main.py
+++ main.py
@@ -106,27 +106,28 @@ class ComputerCar(AbstractCar):
         target_x, target_y = self.path[self.current_point]
         x_diff = target_x - self.x
         y_diff = target_y - self.y
+
         if y_diff == 0:
-            desired_radian_angle = math.pi/2
+            desired_radian_angle = math.pi / 2
         else:
-            desired_radian_angle = math.atan(x_diff/y_diff) 
-    
-        if targer_y > self.y:
+            desired_radian_angle = math.atan(x_diff / y_diff)
+
+        if target_y > self.y:
             desired_radian_angle += math.pi
-            
+
         difference_in_angle = self.angle - math.degrees(desired_radian_angle)
         if difference_in_angle >= 180:
             difference_in_angle -= 360
-            
-        #rotate the computer car to let it move toward the point
-        if deference_in_angle > 0:
-            self.angle -= min(self.rotation_vel, abs(difference_in_angle)) 
+
+        if difference_in_angle > 0:
+            self.angle -= min(self.rotation_vel, abs(difference_in_angle))
         else:
             self.angle += min(self.rotation_vel, abs(difference_in_angle))
     
     def update_path_point(self):
         target = self.path[self.current_point]
-        rect = pygame.Rect(self.x, self.y, self.img.get_width(), self.img.get_height())
+        rect = pygame.Rect(
+            self.x, self.y, self.img.get_width(), self.img.get_height())
         if rect.collidepoint(*target):
             self.current_point += 1
     
@@ -134,7 +135,7 @@ class ComputerCar(AbstractCar):
     def move(self):
         if self.current_point >= len(self.path):
             return
-        
+
         self.calculate_angle()
         self.update_path_point()
         super().move()
@@ -168,7 +169,7 @@ run = True
 clock = pygame.time.Clock()
 images = [(GRASS, (0,0)), (TRACK, (0,0)), (FINISH, FINISH_POSITION), (TRACK_BORDER, (0,0))]
 player_car = PlayerCar(4, 4)
-computer_car = ComputerCar(4, 4)
+computer_car = ComputerCar(4, 4, PATH)
 
 
 while run:
