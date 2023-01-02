4ee8658c11b73a86624da32f6fd252f2e94d1001
AidenKietTran <91713063+AidenKietTran@users.noreply.github.com>
2023-01-02 23:30:37 +1100

    testing commit account

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ pygame.display.set_caption("Racing Game!")
 
 MAIN_FONT = pygame.font.SysFont("comicsans", 44)
 
-FPS = 60
+FPS = 80
 PATH = [(175, 119), (110, 70), (56, 133), (70, 481), (318, 731), (404, 680), (418, 521), (507, 475), (600, 551), (613, 715), (736, 713),
         (734, 399), (611, 357), (409, 343), (433, 257), (697, 258), (738, 123), (581, 71), (303, 78), (275, 377), (176, 388), (178, 260)]
 
@@ -239,8 +239,8 @@ def handle_collision(player_car, computer_car, game_info):
 run = True
 clock = pygame.time.Clock()
 images = [(GRASS, (0,0)), (TRACK, (0,0)), (FINISH, FINISH_POSITION), (TRACK_BORDER, (0,0))]
-player_car = PlayerCar(4, 4)
-computer_car = ComputerCar(2, 4, PATH)
+player_car = PlayerCar(2, 4)
+computer_car = ComputerCar(1, 4, PATH)
 game_info = GameInfo()
 
 
@@ -275,7 +275,6 @@ while run:
         game_info.reset()
         player_car.reset()
         computer_car.reset()
-
 
 
   
